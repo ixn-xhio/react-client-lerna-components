8e56edd017c56716fa3a0b71519927d7b1839ef7
ian xhio <xhio.ai@MacBook-Pro-de-Ian.local>
2023-12-30 01:48:22 -0600

    work completed

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { defineCustomElements } from '@workspace/react';
-defineCustomElements().then(() => console.log("React Stencil Loaded Successfully"));
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
-    <App />
-  </React.StrictMode>);
